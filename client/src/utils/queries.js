6665de8d429c82a2afc36ff3e9287fc3aed98cbe
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-10-06 21:39:06 -0400

    update client side query and test development

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,17 +1,19 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-    me {
-        _id
-        username
-        email
-        savedBooks {
-            title
-            authors
-            description
-            image
-            link
-            bookId
+    {
+        me {
+            _id
+            username
+            email
+            savedBooks {
+                title
+                authors
+                description
+                image
+                link
+                bookId
+            }
         }
     }
 `
